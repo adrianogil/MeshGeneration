b333c73363203a2a12aa1f2dd8c97082af8b1bd9
Adriano Gil <adriano.gil@indt.org.br>
2016-02-16 08:12:09 -0400

    Generating a dodecahedron

---
+++ Assets/Scripts/Dodecahedron.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class Dodecahedron : MonoBehaviour {
+
+	public List<Vector3> vertices = new List<Vector3>();
+
+	// Use this for initialization
+	void Start () {
+		MeshBuilder meshBuilder = new MeshBuilder();
+
+		meshBuilder.Vertices.Add(new Vector3(-1f,-1f,-1f)); // 0
+		meshBuilder.Vertices.Add(new Vector3(-1f,-1f, 1f)); // 1
+		meshBuilder.Vertices.Add(new Vector3(-1f, 1f,-1f)); // 2
+		meshBuilder.Vertices.Add(new Vector3(-1f, 1f, 1f)); // 3
+		meshBuilder.Vertices.Add(new Vector3( 1f,-1f,-1f)); // 4
+		meshBuilder.Vertices.Add(new Vector3( 1f,-1f, 1f)); // 5
+		meshBuilder.Vertices.Add(new Vector3( 1f, 1f,-1f)); // 6
+		meshBuilder.Vertices.Add(new Vector3( 1f, 1f, 1f)); // 7
+
+		float phi = (1f + Mathf.Sqrt(5f))/2f;
+
+		meshBuilder.Vertices.Add(new Vector3(0f, -1f/phi, -phi)); // 8
+		meshBuilder.Vertices.Add(new Vector3(0f, -1f/phi,  phi)); // 9
+		meshBuilder.Vertices.Add(new Vector3(0f,  1f/phi, -phi)); // 10
+		meshBuilder.Vertices.Add(new Vector3(0f,  1f/phi,  phi)); // 11
+
+
+		meshBuilder.Vertices.Add(new Vector3(-1f/phi, -phi, 0f)); // 12
+		meshBuilder.Vertices.Add(new Vector3(-1f/phi,  phi, 0f)); // 13
+		meshBuilder.Vertices.Add(new Vector3( 1f/phi, -phi, 0f)); // 14
+		meshBuilder.Vertices.Add(new Vector3( 1f/phi,  phi, 0f)); // 15
+
+		meshBuilder.Vertices.Add(new Vector3(-phi, 0f, -1f/phi)); // 16
+		meshBuilder.Vertices.Add(new Vector3( phi, 0f, -1f/phi)); // 17
+		meshBuilder.Vertices.Add(new Vector3(-phi, 0f,  1f/phi)); // 18
+		meshBuilder.Vertices.Add(new Vector3( phi, 0f,  1f/phi)); // 19
+
+		vertices = meshBuilder.Vertices;
+
+		AddDodecahedronFace(meshBuilder, 7, 19, 17, 6, 15);
+		AddDodecahedronFace(meshBuilder, 6, 17, 4, 8, 10);
+		AddDodecahedronFace(meshBuilder, 15, 6, 10, 2, 13);
+		AddDodecahedronFace(meshBuilder, 2, 10, 8, 0, 16);
+		AddDodecahedronFace(meshBuilder, 13, 2, 16, 18, 3);
+		AddDodecahedronFace(meshBuilder, 7, 15, 13, 3, 11);
+		AddDodecahedronFace(meshBuilder, 4, 17, 19, 5, 14);
+		AddDodecahedronFace(meshBuilder, 0, 8, 4, 14, 12);
+		AddDodecahedronFace(meshBuilder, 18, 16, 0, 12, 1);
+		AddDodecahedronFace(meshBuilder, 11, 3, 18, 1, 9);
+		AddDodecahedronFace(meshBuilder, 19, 7, 11, 9, 5);
+		AddDodecahedronFace(meshBuilder, 5, 9, 1, 12, 14);
+
+		AddInternalCubeFace(meshBuilder, 2, 6, 4, 0);
+		AddInternalCubeFace(meshBuilder, 6, 7, 5, 4);
+		AddInternalCubeFace(meshBuilder, 7, 3, 1, 5);
+		AddInternalCubeFace(meshBuilder, 3, 2, 0, 1);
+		AddInternalCubeFace(meshBuilder, 3, 7, 6, 2);
+		AddInternalCubeFace(meshBuilder, 5, 1, 0, 4);
+
+		Mesh mesh = GetComponent<MeshFilter> ().mesh = meshBuilder.CreateMesh ();
+
+		mesh.RecalculateBounds ();
+		mesh.RecalculateNormals ();
+	}
+	
+	void AddDodecahedronFace(MeshBuilder meshBuilder, int v1, int v2, int v3, int v4, int v5)
+	{
+		meshBuilder.AddTriangle(v1, v4, v5);
+		meshBuilder.AddTriangle(v2, v4, v1);
+		meshBuilder.AddTriangle(v3, v4, v2);
+	}
+
+	void AddInternalCubeFace(MeshBuilder meshBuilder, int v1, int v2, int v3, int v4)
+	{
+		meshBuilder.AddTriangle(v4, v3, v1);
+		meshBuilder.AddTriangle(v3, v2, v1);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.black;
+		foreach (Vector3 vertice in vertices) {
+			Gizmos.DrawSphere(vertice, 0.1f);
+		}
+	}
+}
---
+++ Assets/Scripts/MeshPlayground.cs
@@ -0,0 +1,178 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class MeshPlayground : MonoBehaviour {
+
+	public float meshLength, meshWidth, meshHeight;
+
+	public int sizeX, sizeZ;
+
+	public float animationDuration;
+
+	private Vector3[] cubeVertices;
+	private Vector3[] sphereVertices;
+	private Vector3[] vertices;
+
+	private float progress;
+
+	private Mesh mesh;
+
+	void PrepareForVerticesAnimation() {
+		mesh = GetComponent<MeshFilter> ().mesh;
+		mesh.MarkDynamic ();
+
+		cubeVertices = mesh.vertices;
+
+		float radius = 2 * meshLength;
+
+		sphereVertices = new Vector3[cubeVertices.Length];
+
+		for (int i = 0; i < cubeVertices.Length; i++) {
+			sphereVertices[i] = cubeVertices[i].normalized * radius;
+		}
+
+		vertices = mesh.vertices;
+	}
+
+	void Update() {
+		progress += Time.deltaTime / animationDuration;
+
+		if (progress >= 1f) {
+			progress -= 1f;
+		}
+
+		// Animacao de Vertices
+		for (int i = 0; i < vertices.Length; i++) {
+			vertices[i] = cubeVertices[i] + progress * (sphereVertices[i] - cubeVertices[i]);
+		}
+
+		mesh.vertices = vertices;
+		mesh.RecalculateBounds ();
+		mesh.RecalculateNormals ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		GenerateCubeMesh ();
+	}
+
+	void GenerateQuadMesh() {
+		MeshBuilder meshBuilder = new MeshBuilder ();
+
+		float vx, vz;
+		Vector3 offset;
+
+		for (int z = 0; z < sizeZ; z++) {
+			vz = meshLength * z;
+
+			for (int x = 0; x < sizeX; x++) {
+				vx = meshWidth * x;
+
+				offset = new Vector3(vx, Random.Range (0f, meshHeight), vz);
+
+				BuildQuad(meshBuilder, offset);
+			}
+		}
+
+		GetComponent<MeshFilter> ().mesh = meshBuilder.CreateMesh ();
+	}
+
+	void BuildQuad(MeshBuilder meshBuilder, Vector3 offset) {
+
+
+		meshBuilder.Vertices.Add (new Vector3(0f, 0f, 0f) + offset);
+		meshBuilder.UVs.Add (new Vector2(0f, 0f));
+		meshBuilder.Normals.Add (Vector3.up);
+
+		meshBuilder.Vertices.Add (new Vector3(0f, 0f, meshLength) + offset);
+		meshBuilder.UVs.Add (new Vector2(0f, 1f));
+		meshBuilder.Normals.Add (Vector3.up);
+
+		meshBuilder.Vertices.Add (new Vector3(meshWidth, 0f, meshLength) + offset);
+		meshBuilder.UVs.Add (new Vector2(1f, 1f));
+		meshBuilder.Normals.Add (Vector3.up);
+
+		meshBuilder.Vertices.Add (new Vector3(meshWidth, 0f, 0f) + offset);
+		meshBuilder.UVs.Add (new Vector2(1f, 0f));
+		meshBuilder.Normals.Add (Vector3.up);
+
+		int baseIndex = meshBuilder.Vertices.Count - 4;
+
+		meshBuilder.AddTriangle (baseIndex, baseIndex+1, baseIndex+2);
+		meshBuilder.AddTriangle (baseIndex, baseIndex+2, baseIndex+3);
+	}
+
+	void BuildQuad(MeshBuilder meshBuilder, Vector3 offset, Vector3 widthDir, Vector3 lengthDir, int sizeX, int sizeY) {
+
+		Vector3 normal = Vector3.Cross (lengthDir, widthDir).normalized;
+
+		float vx, vy;
+
+		// Define vertices e triangulos
+		for (int y = 0; y <= sizeY; y++) {
+			for (int x = 0; x <= sizeX; x++) {
+				vx = ((float) x / sizeX);
+				vy = ((float) y / sizeY);
+
+				meshBuilder.Vertices.Add (offset + vx * lengthDir + vy * widthDir);
+				meshBuilder.UVs.Add (new Vector2(vx, vy));
+				meshBuilder.Normals.Add (normal);
+			}
+		}
+		
+		int baseIndex = meshBuilder.Vertices.Count - (sizeX+1) * (sizeY+1);
+
+		for (int vi = baseIndex, y = 0; y < sizeY; y++, vi++) {
+			for (int x = 0; x < sizeX; x++, vi++) {
+				meshBuilder.AddTriangle (vi, vi+1, vi+sizeX+2);
+				meshBuilder.AddTriangle (vi, vi+sizeX+2, vi+sizeX+1);
+			}
+		}
+
+	}
+
+	void GenerateCubeMesh() {
+		MeshBuilder meshBuilder = new MeshBuilder ();
+
+		Vector3 upDir = Vector3.up * meshHeight;
+		Vector3 rightDir = Vector3.right * meshWidth;
+		Vector3 forwardDir = Vector3.forward * meshLength;
+
+		Vector3 nearCorner = - 0.5f * (upDir + rightDir + forwardDir);
+		Vector3 farCorner =  nearCorner + upDir + rightDir + forwardDir;
+
+		BuildQuad (meshBuilder, nearCorner, forwardDir, rightDir, sizeX, sizeZ);
+		BuildQuad (meshBuilder, nearCorner, rightDir, upDir, sizeX, sizeZ);
+		BuildQuad (meshBuilder, nearCorner, upDir, forwardDir, sizeX, sizeZ);
+
+		BuildQuad (meshBuilder, farCorner, -rightDir, -forwardDir, sizeX, sizeZ);
+		BuildQuad (meshBuilder, farCorner, -upDir, -rightDir, sizeX, sizeZ);
+		BuildQuad (meshBuilder, farCorner, -forwardDir, -upDir, sizeX, sizeZ);
+
+		GetComponent<MeshFilter> ().mesh = meshBuilder.CreateMesh ();
+
+		//Invoke ("Spherify", 3f);
+
+		PrepareForVerticesAnimation ();
+	}
+
+	void Spherify() {
+		Mesh mesh = GetComponent<MeshFilter> ().mesh;
+		mesh.MarkDynamic ();
+
+		float radius = meshHeight * 2;
+
+		Vector3[] vertices = mesh.vertices;
+
+		for (int i = 0; i < vertices.Length; i++) {
+			vertices[i] = vertices[i].normalized * radius;
+		}
+
+		mesh.vertices = vertices;
+
+		mesh.RecalculateBounds ();
+		mesh.RecalculateNormals ();
+
+	}
+}
